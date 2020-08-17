eeac582373eab0cce24857d3bb7b1f2060a47b26
zhangaz1 <zhangaz1@hotmail.com>
2020-08-17 15:52:47 +0800

    import libs in utils

---
+++ packages/libs/README.md
@@ -0,0 +1,6 @@
+# @dev-tool-kit/libs
+
+import common libs etc:
+
+ramda as fp
+antd ad ui
---
+++ packages/libs/src/index.ts
@@ -0,0 +1,5 @@
+export * as fp from 'ramda';
+
+export function add(a: number, b: number) {
+	return a + b;
+}
