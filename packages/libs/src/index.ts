63981b9043cb08720a720310826352ab7ee54aeb
zhangaz1 <zhangaz1@hotmail.com>
2020-08-25 07:50:17 +0800

    add debug to libs

--- packages/libs/src/index.ts
+++ packages/libs/src/index.ts
@@ -1,7 +1,14 @@
 import * as fp from 'ramda';
+import createDebug from 'debug';
 
-export { fp };
+export {
+	fp,
+	createDebug,
+};
+
+const debug = createDebug('lib:index');
 
 export function add(a: number, b: number) {
+	debug('args:', arguments);
 	return a + b;
 }
